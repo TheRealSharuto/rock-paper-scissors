6a001833e6b742207b808de31a88369f3c563e35
Shara <85804934+TheRealSharuto@users.noreply.github.com>
2023-12-04 15:06:32 -0500

    Creating rock paper scissors project html+ css+js

---
+++ main.js
@@ -0,0 +1,60 @@
+/* Objects: Wins, Losses, and Ties counters */
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+/*
+Lets the user throw a rock, paper, or scissor depending on which button they click.
+Lets computer compete against the user by randomly choosing rock, paper, or scissors
+*/
+document.getElementById("rock").addEventListener("click", computerThinks);
+document.getElementById("paper").addEventListener("click", computerThinks);
+document.getElementById("scissors").addEventListener("click", computerThinks);
+
+function computerThinks() {
+    const computersNumber = Math.random();
+    let result = '';
+    let computerShoots = '';
+    let userChoice = this.id;
+
+    if (computersNumber >= 0 && computersNumber < 1 / 3) {
+        computerShoots = 'rock';
+    } else if (computersNumber >= 0 && 2 / 3 < computersNumber >= 1 / 3) {
+        computerShoots = 'paper';
+    } else if (computersNumber >= 0 && 3 >= computersNumber > 2 / 3) {
+        computerShoots = 'scissors';
+    }
+
+    console.log(`Computer thinks and chooses: ${computerShoots}.`);
+
+    if (computerShoots === userChoice) {
+        result = 'Tie.';
+    } else if ((computerShoots === 'paper' && userChoice === 'rock') || (computerShoots === 'rock' && userChoice === 'scissors') || (computerShoots === 'scissors' && userChoice === 'paper')) {
+        result = 'You lose.';
+    } else if ((computerShoots === 'rock' && userChoice === 'paper') || (computerShoots === 'paper' && userChoice === 'scissors') || (computerShoots === 'scissors' && userChoice === 'rock')) {
+        result = 'You win.';
+    }
+
+    saveResults(result);
+
+    alert(`You picked ${userChoice}. Computer picked ${computerShoots}. ${result}.
+    Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}.`);
+
+}
+
+function saveResults(result){
+    switch(result) {
+        case 'You win':
+            score.wins += 1;
+            break;
+        case 'You lose':
+            score.loses += 1;
+            break;
+        case 'Tie':
+            score.tie += 1;
+        default:
+            'There has been an error in the game.';
+    }
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  './rock-paper-scissors.html', 
+  './main.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
